6de7dde8563a68cf49e654a68f6116d69cd0dd1a
Chirag Goel <chirag@Chirags-MacBook-Pro.local>
2019-11-19 19:33:51 -0500

    custom info

---
+++ users/custom_data_to_csv.py
@@ -0,0 +1,24 @@
+import csv
+
+
+finalData = []
+with open('../data/custom_data.csv', newline='') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',')
+		for row in reader:
+			flag = False
+			for row in finalData:
+				if(row["name"] == row[1]):
+					row["records"].append(row[0])
+					flag = True
+					break;
+			if(!flag):
+				user = {}
+				user["name"] = row[1]
+				user["records"] = [row[0]]
+				finalData.append(user)
+
+
+with open("train_balanced_user_custom.csv", 'w') as output_file:
+        writer = csv.writer(output_file, quoting=csv.QUOTE_ALL)
+        writer.writerows((user["name"], " <END> ".join(user["records"]))
+                         for user in iteritems(finalData))
