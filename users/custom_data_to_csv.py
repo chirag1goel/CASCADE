c55234d8db84aff1be113289958ee167e07dcf65
Chirag Goel <chirag@Chirags-MacBook-Pro.local>
2019-11-19 19:43:22 -0500

    custom data file updated

--- users/custom_data_to_csv.py
+++ users/custom_data_to_csv.py
@@ -3,17 +3,18 @@ import csv
 
 finalData = []
 with open('../data/custom_data.csv', newline='') as csvfile:
-	reader = csv.reader(csvfile, delimiter=',')
-	for row in reader:
+	for row in csv.reader((line.replace('\0','') for line in csvfile), delimiter=","):
+	# for row in reader:
 		flag = True
 		for user in finalData:
-			if(user["name"] == row[1]):
+			if(user["name"] == row[2]):
 				user["records"].append(row[0])
 				flag = False
 				break;
 		if(flag):
+			print("new user : " + row[2])
 			user = {}
-			user["name"] = row[1]
+			user["name"] = row[2]
 			user["records"] = [row[0]]
 			finalData.append(user)
 
@@ -21,4 +22,4 @@ with open('../data/custom_data.csv', newline='') as csvfile:
 with open("train_balanced_user_custom.csv", 'w') as output_file:
         writer = csv.writer(output_file, quoting=csv.QUOTE_ALL)
         writer.writerows((user["name"], " <END> ".join(user["records"]))
-                         for user in iteritems(finalData))
+                         for user in finalData)
